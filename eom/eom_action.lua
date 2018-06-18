baff70927d999d13503359b6b30665d94977cefa
DrunkFlamingo <matte901@gmail.com>
2018-06-18 16:30:32 -0400

    finished action object

--- eom/eom_action.lua
+++ eom/eom_action.lua
@@ -36,7 +36,9 @@ end
 
 --v function(self: EOM_ACTION, choice: number)
 function eom_action.do_choice(self, choice)
-    self._choices[choice](self:model())
+    EOMLOG("Doing choice callback ["..tostring(choice).."] for event ["..self:key().."] ")
+    local choice_callback = self._choices[choice]
+    choice_callback(self:model())
 end
 
 --v function(self: EOM_ACTION) --> boolean
@@ -48,5 +50,18 @@ end
 function eom_action.act(self)
     cm:trigger_dilemma(EOM_GLOBAL_EMPIRE_FACTION, self:key(), true)
     cm:set_saved_value("eom_action_"..self:key().."_occured", true)
+    core:add_listener(
+        self:key(),
+        "DilemmaChoiceMadeEvent",
+        function(context)
+           return context:faction():name() == EOM_GLOBAL_EMPIRE_FACTION
+        end,
+        function(context)
+            self:do_choice((context:choice()+1))
+        end,
+        false)
 end
 
+return {
+    new = eom_action.new
+}
