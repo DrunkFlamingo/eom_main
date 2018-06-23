fe2f1299ecf58d17485a7bef73e6ece259d959d6
DrunkFlamingo <matte901@gmail.com>
2018-06-23 11:23:03 -0400

    moved core triggers to a seperate export helper

--- export_helpers__eom_battles.lua
+++ export_helpers__eom_battles.lua
@@ -1,4 +1,28 @@
-cm = get_cm() events = get_events() eom = _G.eom
+cm = get_cm(); events = get_events(); eom = _G.eom;
+
+
+core:add_listener(
+    "EOMTurnStart",
+    "FactionTurnStart",
+    function(context)
+        local faction = context:faction()
+        return faction:name() == EOM_GLOBAL_EMPIRE_FACTION
+    end,
+    function(context)
+        eom:event_and_plot_check()
+        eom:elector_diplomacy()
+    end,
+    true);
+
+
+
+
+
+
+
+
+
+
 
 core:add_listener(
     "EOMVampiresDefeated",
---
+++ export_helpers__eom_core.lua
@@ -0,0 +1,14 @@
+cm = get_cm(); events = get_events(); eom = _G.eom;
+
+core:add_listener(
+    "EOMTurnStart",
+    "FactionTurnStart",
+    function(context)
+        local faction = context:faction()
+        return faction:name() == "wh_main_emp_empire"
+    end,
+    function(context)
+        eom:event_and_plot_check()
+        eom:elector_diplomacy()
+    end,
+    true);
