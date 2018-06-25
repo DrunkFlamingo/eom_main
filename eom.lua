c0f719ad50de8b3e569b1424dfb9d01c88db75b6
DrunkFlamingo <matte901@gmail.com>
2018-06-25 10:56:28 -0400

    recompiles

--- eom.lua
+++ eom.lua
@@ -1,3 +1,166 @@
+
+--[[
+--v function(text: string)
+function EOM_ERRORS(text)
+    ftext = "debugger"
+    --sometimes I use ftext as an arg of this function, but for simple mods like this one I don't need it.
+
+    if not EOM_SHOULD_LOG then
+        return; --if our bool isn't set true, we don't want to spam the end user with logs. 
+    end
+
+    local logText = tostring(text)
+    local logContext = tostring(ftext)
+    local logTimeStamp = os.date("%d, %m %Y %X")
+    local popLog = io.open("EOM_DEBUGG.txt","a")
+    --# assume logTimeStamp: string
+    popLog :write("WEC:  "..logText .. "    : [" .. logContext .. "] : [".. logTimeStamp .. "]\n")
+    popLog :flush()
+    popLog :close()
+end
+
+--v [NO_CHECK] function(func: function) --> any
+function safeCall(func)
+    --RCDEBUG("safeCall start");
+    local status, result = pcall(func)
+    if not status then
+        EOM_ERRORS(tostring(result))
+        EOM_ERRORS(debug.traceback());
+    end
+    --RCDEBUG("safeCall end");
+    return result;
+end
+
+--local oldTriggerEvent = core.trigger_event;
+
+--v [NO_CHECK] function(...: any)
+function pack2(...) return {n=select('#', ...), ...} end
+--v [NO_CHECK] function(t: vector<WHATEVER>) --> vector<WHATEVER>
+function unpack2(t) return unpack(t, 1, t.n) end
+
+--v [NO_CHECK] function(f: function(), argProcessor: function()) --> function()
+function wrapFunction(f, argProcessor)
+    return function(...)
+        --RCDEBUG("start wrap ");
+        local someArguments = pack2(...);
+        if argProcessor then
+            safeCall(function() argProcessor(someArguments) end)
+        end
+        local result = pack2(safeCall(function() return f(unpack2( someArguments )) end));
+        --for k, v in pairs(result) do
+        --    RCDEBUG("Result: " .. tostring(k) .. " value: " .. tostring(v));
+        --end
+        --RCDEBUG("end wrap ");
+        return unpack2(result);
+        end
+end
+
+-- function myTriggerEvent(event, ...)
+--     local someArguments = { ... }
+--     safeCall(function() oldTriggerEvent(event, unpack( someArguments )) end);
+-- end
+
+--v [NO_CHECK] function(fileName: string)
+function tryRequire(fileName)
+    local loaded_file = loadfile(fileName);
+    if not loaded_file then
+        EOM_ERRORS("Failed to find mod file with name " .. fileName)
+    else
+        EOM_ERRORS("Found mod file with name " .. fileName)
+        EOM_ERRORS("Load start")
+        local local_env = getfenv(1);
+        setfenv(loaded_file, local_env);
+        loaded_file();
+        EOM_ERRORS("Load end")
+    end
+end
+
+core:add_listener(
+    "LaunchRuntimeScript",
+    "ShortcutTriggered",
+    function(context) return context.string == "camera_bookmark_view1"; end, --default F10
+    function(context)
+        tryRequire("test");
+    end,
+    true
+);
+
+--v [NO_CHECK] function(f: function(), name: string)
+function logFunctionCall(f, name)
+    return function(...)
+        EOM_ERRORS("function called: " .. name);
+        return f(...);
+    end
+end
+
+--v [NO_CHECK] function(object: any)
+function logAllObjectCalls(object)
+    local metatable = getmetatable(object);
+    for name,f in pairs(getmetatable(object)) do
+        if is_function(f) then
+            EOM_ERRORS("Found " .. name);
+            if name == "Id" or name == "Parent" or name == "Find" or name == "Position" or name == "CurrentState"  or name == "Visible"  or name == "Priority" or "Bounds" then
+                --Skip
+            else
+                metatable[name] = logFunctionCall(f, name);
+            end
+        end
+        if name == "__index" and not is_function(f) then
+            for indexname,indexf in pairs(f) do
+                EOM_ERRORS("Found in index " .. indexname);
+                if is_function(indexf) then
+                    f[indexname] = logFunctionCall(indexf, indexname);
+                end
+            end
+            EOM_ERRORS("Index end");
+        end
+    end
+end
+
+-- logAllObjectCalls(core);
+-- logAllObjectCalls(cm);
+-- logAllObjectCalls(game_interface);
+
+core.trigger_event = wrapFunction(
+    core.trigger_event,
+    function(ab)
+        --RCDEBUG("trigger_event")
+        --for i, v in pairs(ab) do
+        --    RCDEBUG("i: " .. tostring(i) .. " v: " .. tostring(v))
+        --end
+        --RCDEBUG("Trigger event: " .. ab[1])
+    end
+);
+
+cm.check_callbacks = wrapFunction(
+    cm.check_callbacks,
+    function(ab)
+        --RCDEBUG("check_callbacks")
+        --for i, v in pairs(ab) do
+        --    RCDEBUG("i: " .. tostring(i) .. " v: " .. tostring(v))
+        --end
+    end
+)
+
+local currentAddListener = core.add_listener;
+--v [NO_CHECK] function(core: any, listenerName: any, eventName: any, conditionFunc: any, listenerFunc: any, persistent: any)
+function myAddListener(core, listenerName, eventName, conditionFunc, listenerFunc, persistent)
+    local wrappedCondition = nil;
+    if is_function(conditionFunc) then
+        --wrappedCondition =  wrapFunction(conditionFunc, function(arg) RCDEBUG("Callback condition called: " .. listenerName .. ", for event: " .. eventName); end);
+        wrappedCondition =  wrapFunction(conditionFunc);
+    else
+        wrappedCondition = conditionFunc;
+    end
+    currentAddListener(
+        core, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc), persistent
+        --core, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc, function(arg) RCDEBUG("Callback called: " .. listenerName .. ", for event: " .. eventName); end), persistent
+    )
+end
+core.add_listener = myAddListener;
+--]]
+
+
 EOM_SHOULD_LOG = true --:boolean
 EOM_LAST_CONTEXT = "no context set" --:string
 
@@ -95,6 +258,7 @@ end
 
 
 
+
 --global variables
 
 --reduce typing
@@ -155,7 +319,6 @@ EOM_GLOBAL_REGION_TO_ELECTOR = {
     ["wh_main_the_wasteland_gorssel"] = "wh_main_emp_marienburg",
     ["wh_main_the_wasteland_marienburg"] = "wh_main_emp_marienburg"
 }--:map<string, ELECTOR_NAME>
-
 
 --v function() --> string
 local function get_default_start_army()
@@ -642,6 +805,141 @@ function return_elector_starts()
 end;
 
 
+local eom_plot = {} --# assume eom_plot: EOM_PLOT
+
+--v function(name: string, model: EOM_MODEL) --> EOM_PLOT
+function eom_plot.new(name, model)
+local self = {}
+setmetatable(self, {
+    __index = eom_plot
+}) --# assume self: EOM_PLOT
+
+self._name = name
+self._model = model 
+if cm:get_saved_value("plot_line_stage"..name) then
+    self._current_stage = cm:get_saved_value("plot_line_stage"..name) --:int
+    EOMLOG("Loaded stage ["..tostring(self._current_stage).."] for plot element ["..name.."]")
+else
+    self._current_stage = 0
+end
+self._triggers = {} --:vector<function(eom: EOM_MODEL) --> boolean>
+self._callbacks = {} --:vector<function(eom: EOM_MODEL)>
+if cm:get_saved_value("plot_line_ended_"..name) == nil then
+    cm:set_saved_value("civil_war_ended"..name,false) 
+end
+
+
+return self
+end
+
+--v function(self: EOM_PLOT) --> string
+function eom_plot.name(self)
+    return self._name
+end
+
+--v function(self: EOM_PLOT) --> EOM_MODEL
+function eom_plot.model(self)
+    return self._model
+end
+
+--v function(self: EOM_PLOT) --> int
+function eom_plot.current_stage(self)
+    return self._current_stage
+end
+
+--v function(self: EOM_PLOT, stage: int)
+function eom_plot.set_stage(self, stage)
+    EOMLOG("Advancing stage for civil war ["..self:name().."] to ["..tostring(stage).."] ")
+    self._current_stage = stage
+    cm:set_saved_value("plot_line_stage"..self:name(), self:current_stage())
+end
+
+--v function(self: EOM_PLOT, stage: integer) --> boolean
+function eom_plot.has_stage(self, stage)
+    return not not self._triggers[stage]
+end
+
+--v function(self: EOM_PLOT, stage: integer)
+function eom_plot.stage_callback(self, stage)
+    if not self._callbacks[stage] then
+        EOMLOG("No callbacks for stage ["..tostring(stage).."] in civil war ["..self:name().."] ")
+        return
+    end
+    local stage_callback = self._callbacks[stage]
+    stage_callback(self:model())
+end
+
+
+--v function(self: EOM_PLOT, stage: integer) --> boolean
+function eom_plot.check_trigger(self, stage)
+    local stage_trigger = self._triggers[stage]
+    return stage_trigger(self:model())
+end
+
+
+
+--v function(self: EOM_PLOT) --> boolean
+function eom_plot.is_over(self)
+    return cm:get_saved_value("plot_line_ended_"..self:name())
+end
+
+--v function(self: EOM_PLOT) --> boolean
+function eom_plot.is_active(self)
+    return (not cm:get_saved_value("plot_line_ended_"..self:name())) and (self:current_stage() > 0)
+end
+
+
+--v [NO_CHECK] function(self: EOM_PLOT)--> boolean
+function eom_plot.check_advancement(self) 
+    if self:is_over() then
+        EOMLOG("Checked advancement for civil war ["..self:name().."] but that civil war is over! ")
+        return false
+    end
+    if self:model():get_core_data_with_key("test_all_plot_events") == true then
+        return true
+    end
+    local c_stage = self:current_stage()
+    local n_stage = c_stage + 1;
+    if self:has_stage(n_stage) and self:check_trigger(n_stage) then
+        return true
+    else 
+        EOMLOG("Civil war ["..self:name().."] is not advancing")
+        return false
+    end
+end
+
+--v function (self: EOM_PLOT)
+function eom_plot.advance(self)
+    local c_stage = self:current_stage()
+    local n_stage = c_stage + 1;
+    self:set_stage(n_stage)
+    self:stage_callback(n_stage)
+end
+
+--v function(self: EOM_PLOT, stage: int, trigger: function(model: EOM_MODEL) --> boolean)
+function eom_plot.add_stage_trigger(self, stage, trigger)
+EOMLOG("Added trigger for stage ["..tostring(stage).."] in plot line ["..self:name().."]")
+    self._triggers[stage] = trigger
+end
+--v function(self: EOM_PLOT, stage: int, callback: function(model: EOM_MODEL))
+function eom_plot.add_stage_callback(self, stage, callback)
+    EOMLOG("Added callback for stage ["..tostring(stage).."] in plot line ["..self:name().."]")
+    self._callbacks[stage] = callback
+end
+
+--v function(self: EOM_PLOT)
+function eom_plot.finish(self)
+    cm:set_saved_value("plot_line_ended_"..self:name(), true)
+end
+
+--v function(self: EOM_PLOT)
+function eom_plot.save(self)
+    cm:set_saved_value("plot_line_stage"..self:name(), self:current_stage())
+end
+
+
+
+
 local eom_action = {} --# assume eom_action: EOM_ACTION
 
 --v function(key: string, conditional: function(eom: EOM_MODEL) --> boolean, choices: map<number, function(eom: EOM_MODEL)>, eom: EOM_MODEL) --> EOM_ACTION
@@ -705,6 +1003,7 @@ function eom_action.act(self)
         end,
         false)
 end
+
 
 local eom_elector = {} --# assume eom_elector: EOM_ELECTOR
 
@@ -1002,6 +1301,8 @@ function eom_elector.home_regions(self)
     return self._homeRegions
 end
 
+
+
 --revival system
 
 --v function(self: EOM_ELECTOR) --> number
@@ -1150,139 +1451,6 @@ function eom_elector.set_fully_loyal(self, model)
     cm:trigger_incident(EOM_GLOBAL_EMPIRE_FACTION, "eom_full_loyalty_"..self:name(), true)
     cm:force_confederation(EOM_GLOBAL_EMPIRE_FACTION, self:name())
 end
-
-local eom_plot = {} --# assume eom_plot: EOM_PLOT
-
---v function(name: string, model: EOM_MODEL) --> EOM_PLOT
-function eom_plot.new(name, model)
-local self = {}
-setmetatable(self, {
-    __index = eom_plot
-}) --# assume self: EOM_PLOT
-
-self._name = name
-self._model = model 
-if cm:get_saved_value("plot_line_stage"..name) then
-    self._current_stage = cm:get_saved_value("plot_line_stage"..name) --:int
-    EOMLOG("Loaded stage ["..tostring(self._current_stage).."] for plot element ["..name.."]")
-else
-    self._current_stage = 0
-end
-self._triggers = {} --:vector<function(eom: EOM_MODEL) --> boolean>
-self._callbacks = {} --:vector<function(eom: EOM_MODEL)>
-if cm:get_saved_value("plot_line_ended_"..name) == nil then
-    cm:set_saved_value("civil_war_ended"..name,false) 
-end
-
-
-return self
-end
-
---v function(self: EOM_PLOT) --> string
-function eom_plot.name(self)
-    return self._name
-end
-
---v function(self: EOM_PLOT) --> EOM_MODEL
-function eom_plot.model(self)
-    return self._model
-end
-
---v function(self: EOM_PLOT) --> int
-function eom_plot.current_stage(self)
-    return self._current_stage
-end
-
---v function(self: EOM_PLOT, stage: int)
-function eom_plot.set_stage(self, stage)
-    EOMLOG("Advancing stage for civil war ["..self:name().."] to ["..tostring(stage).."] ")
-    self._current_stage = stage
-    cm:set_saved_value("plot_line_stage"..self:name(), self:current_stage())
-end
-
---v function(self: EOM_PLOT, stage: integer) --> boolean
-function eom_plot.has_stage(self, stage)
-    return not not self._triggers[stage]
-end
-
---v function(self: EOM_PLOT, stage: integer)
-function eom_plot.stage_callback(self, stage)
-    if not self._callbacks[stage] then
-        EOMLOG("No callbacks for stage ["..tostring(stage).."] in civil war ["..self:name().."] ")
-        return
-    end
-    local stage_callback = self._callbacks[stage]
-    stage_callback(self:model())
-end
-
-
---v function(self: EOM_PLOT, stage: integer) --> boolean
-function eom_plot.check_trigger(self, stage)
-    local stage_trigger = self._triggers[stage]
-    return stage_trigger(self:model())
-end
-
-
-
---v function(self: EOM_PLOT) --> boolean
-function eom_plot.is_over(self)
-    return cm:get_saved_value("plot_line_ended_"..self:name())
-end
-
---v function(self: EOM_PLOT) --> boolean
-function eom_plot.is_active(self)
-    return (not cm:get_saved_value("plot_line_ended_"..self:name())) and (self:current_stage() > 0)
-end
-
-
---v [NO_CHECK] function(self: EOM_PLOT)--> boolean
-function eom_plot.check_advancement(self) 
-    if self:is_over() then
-        EOMLOG("Checked advancement for civil war ["..self:name().."] but that civil war is over! ")
-        return false
-    end
-    if self:model():get_core_data_with_key("test_all_plot_events") == true then
-        return true
-    end
-    local c_stage = self:current_stage()
-    local n_stage = c_stage + 1;
-    if self:has_stage(n_stage) and self:check_trigger(n_stage) then
-        return true
-    else 
-        EOMLOG("Civil war ["..self:name().."] is not advancing")
-        return false
-    end
-end
-
---v function (self: EOM_PLOT)
-function eom_plot.advance(self)
-    local c_stage = self:current_stage()
-    local n_stage = c_stage + 1;
-    self:set_stage(n_stage)
-    self:stage_callback(n_stage)
-end
-
---v function(self: EOM_PLOT, stage: int, trigger: function(model: EOM_MODEL) --> boolean)
-function eom_plot.add_stage_trigger(self, stage, trigger)
-EOMLOG("Added trigger for stage ["..tostring(stage).."] in plot line ["..self:name().."]")
-    self._triggers[stage] = trigger
-end
---v function(self: EOM_PLOT, stage: int, callback: function(model: EOM_MODEL))
-function eom_plot.add_stage_callback(self, stage, callback)
-    EOMLOG("Added callback for stage ["..tostring(stage).."] in plot line ["..self:name().."]")
-    self._callbacks[stage] = callback
-end
-
---v function(self: EOM_PLOT)
-function eom_plot.finish(self)
-    cm:set_saved_value("plot_line_ended_"..self:name(), true)
-end
-
---v function(self: EOM_PLOT)
-function eom_plot.save(self)
-    cm:set_saved_value("plot_line_stage"..self:name(), self:current_stage())
-end
-
 
 
 local eom_model = {} --# assume eom_model: EOM_MODEL
@@ -1702,10 +1870,10 @@ function eom_model.event_and_plot_check(self)
     --plot check
     EOMLOG("Core event and plot check function checking story events")
     for key, story in pairs(self:get_story()) do
-        if story:check_advancement() == true then
+       if story:check_advancement() == true then
             story:advance()
             return
-        end
+       end
     end
     --open rebellions
     if not self:get_core_data_with_key("tweaker_no_full_loyalty_events") == true then
@@ -2197,9 +2365,6 @@ function eom_view.docker_button_pressed(self)
         character:SetVisible(false)
     end
 end
-
-
-
 
 
 
