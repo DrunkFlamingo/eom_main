78599b52a5ac4334cc3f91e50d8e6a0eea60b52f
DrunkFlamingo <matte901@gmail.com>
2018-06-26 15:22:11 -0400

    moved a battle listener

--- export_helpers__eom_core.lua
+++ export_helpers__eom_core.lua
@@ -121,31 +121,5 @@ core:add_listener(
     true);
 
 
---event triggers;
-
-
-core:add_listener(
-    "EOMBattlesCompleted",
-    "CharacterCompletedBattle",
-    function(context)
-        local character = context:character() --:CA_CHAR
-        return character:faction():name() == eom:empire() 
-    end,
-    function(context)
-        local character = context:character() --:CA_CHAR
-        local enemies = cm:pending_battle_cache_get_enemies_of_char(character)
-        for i = 1, #enemies do
-            local enemy = enemies[i]
-            local enemy_sub = enemy:faction():subculture()
-            if character:won_battle() then 
-                eom:log("Triggering event VictoryAgainstSubcultureKey_"..enemy_sub, "EOMBattlesCompleted")
-                core:trigger_event("VictoryAgainstSubcultureKey_"..enemy_sub)
-            else
-                core:trigger_event("DefeatAgainstSubcultureKey_"..enemy_sub)
-                eom:log("Triggering event DeafeatAgainstSubcultureKey_"..enemy_sub, "EOMBattlesCompleted")
-            end
-        end
-    end, 
-    true)
 
 
