793249de76c1406b78ee270bff1737cb1e4406c7
DFlunchbreak <matte901@gmail.com>
2018-06-26 12:03:13 -0400

    started on vlad

--- export_helpers__eom_civil_wars.lua
+++ export_helpers__eom_civil_wars.lua
@@ -3,3 +3,28 @@ cm = get_cm(); events = get_events(); eom = _G.eom;
 if not eom then 
     script_error("EOM IS NOT FOUND!")
 end
+
+local function eom_vlad_civil_war()
+
+end
+
+
+
+
+
+
+
+
+
+
+
+local function eom_empire_add_civil_wars()
+    eom:log("Starting", "export_helpers__eom_civil_wars")
+    eom_vlad_civil_war()
+
+
+end
+
+
+
+events.FirstTickAfterWorldCreated[#events.FirstTickAfterWorldCreated+1] = function() eom_empire_add_civil_wars() end;
