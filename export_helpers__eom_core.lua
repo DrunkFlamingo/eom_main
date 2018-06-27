9c567cd45e4de88dd8f404132ffedb52625c05c2
DFlunchbreak <matte901@gmail.com>
2018-06-27 09:54:10 -0400

    added core AI function

--- export_helpers__eom_core.lua
+++ export_helpers__eom_core.lua
@@ -5,7 +5,7 @@ end
 
 
 
-local function empire_plot_and_events_check()
+local function empire_plot_and_events_check_human()
     eom:log("Entered", "eom_model.event_and_plot_check(self)")
 
     if cm:get_saved_value("ci_trigger_mid_game_event_on_turn_start") == true then
@@ -19,6 +19,7 @@ local function empire_plot_and_events_check()
 
 
     --capitulation
+    --unncessary for AI, they capitulate on event
     eom:log("Checking for Electors willing to capitulate")
     for name, elector in pairs(eom:electors()) do
         if elector:will_capitulate() and (not elector:is_cult()) then
@@ -116,6 +117,112 @@ end
 
 
 
+local function empire_plot_and_events_check_ai()
+
+
+    --ai events
+    local next_event = eom:get_core_data_with_key("next_event_turn") --# assume next_event: number
+    if cm:model():turn_number() >= next_event and (not eom:get_core_data_with_key("block_events_for_plot") == true) then
+        eom:log("Core event and plot check function checking political events")
+        for key, event in pairs(eom:events()) do
+            eom:log("Checking Event: ["..key.."] ")
+            if eom:get_core_data_with_key(key.."_occured") ~= true then
+                if event:allowed() then
+                    eom:log("Event ["..key.."] is allowed!")
+                    eom:set_core_data(key.."_occured", true)
+                    event:act_ai()
+                    eom:set_core_data("next_event_turn", cm:model():turn_number() + 5) 
+                    --return
+                    --returning unncessary for the AI.
+                end
+            else
+                eom:log("event ["..key.."] already occured ")
+            end
+        end
+    else
+        eom:log("No event this turn")
+    end
+
+    --full loyalty events.
+    if not eom:get_core_data_with_key("tweaker_no_full_loyalty_events") == true then
+        eom:log("Checking for fully loyal electors")
+        for name, elector in pairs(eom:electors()) do
+            if elector:loyalty() > 99 and elector:status() == "normal" then
+                elector:set_fully_loyal(eom) --function automatically checks humanity.
+            end
+        end
+    end
+
+    --ai restore opportunity.
+    eom:log("Core event and plot check function checking ai restoration opportunities")
+        for name, elector in pairs(eom:electors()) do
+            if not elector:is_cult() then
+                if cm:get_region(elector:capital()):owning_faction():name() == EOM_GLOBAL_EMPIRE_FACTION and cm:get_faction(name):is_dead() then
+                    if elector:status() == "normal" or elector:status() == "open_rebellion" then
+                        if cm:random_number(10) < 3 then
+                            eom:elector_fallen(name)
+                        else
+                            if eom:get_elector(name):status() == "open_rebellion" then
+                                eom:elector_rebellion_end(name)
+                                eom:get_elector(name):respawn_at_capital()
+                                local home_regions = eom:get_elector(name):home_regions()
+                                for i = 1, #home_regions do
+                                    local current_region = home_regions[i]
+                                    if cm:get_region(current_region):owning_faction():subculture() == "wh_main_emp_sc_empire" then
+                                        cm:callback(function()
+                                            cm:transfer_region_to_faction(current_region, name)
+                                        end, i/10)
+                                    end
+                                end
+                            else
+                                eom:get_elector(name):respawn_at_capital()
+                                eom:get_elector(name):change_loyalty(20)
+                                local home_regions = eom:get_elector(name):home_regions()
+                                for i = 1, #home_regions do
+                                    local current_region = home_regions[i]
+                                    if cm:get_region(current_region):owning_faction():subculture() == "wh_main_emp_sc_empire" then
+                                        cm:callback(function()
+                                            cm:transfer_region_to_faction(current_region, name)
+                                        end, i/10)
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        --revival events
+        eom:log("Core event and plot check function checking revivification events")
+        for name, elector in pairs(eom:electors()) do
+            if eom:get_elector_faction(name):is_dead() and elector:turns_dead() > 4 and elector:can_revive() and (not elector:is_cult()) then
+                if cm:get_region(elector:capital()):owning_faction():subculture() == "wh_main_sc_emp_empire" then
+                    elector:trigger_coup(true)
+                    return
+                else
+                    elector:trigger_expedition(true) 
+                    return
+                end
+            end
+        end
+
+
+
+
+        --elector fallen events
+        eom:log("Core event and plot check function checking elector fallen events.")
+        for name, elector in pairs(eom:electors()) do
+            if elector:turns_dead() > 20 and elector:can_revive() == false and (not elector:is_cult()) then
+                eom:elector_fallen(name, false)
+            end
+        end
+
+
+end
+
+
+
 core:add_listener(
     "EOMTurnStart",
     "FactionTurnStart",
@@ -126,7 +233,9 @@ core:add_listener(
     function(context)
         eom:set_log_turn(cm:model():turn_number())
         if cm:get_faction(eom:empire()):is_human() then
-            empire_plot_and_events_check()
+            empire_plot_and_events_check_human()
+        else
+            empire_plot_and_events_check_ai()
         end
     end,
     true);
