d6624d076556dc94bc0eca40a782df981d2c5894
DrunkFlamingo <matte901@gmail.com>
2018-08-04 13:54:00 -0400

    reorganized repo to make it readible for external eyes

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -1,28 +1,26 @@
 
 local production = true --:boolean
 
-require("eom_main")
+require("mod/eom_main")
 --require("df_politics_main")
 
 
 
-require("export_helpers__eom_main_events")
-require("export_helpers__eom_battles")
-require("export_helpers__eom_core")
-require("export_helpers__eom_first_turn")
-require("export_helpers__eom_plot_events")
-require("export_helpers__eom_vlad_civil_wars")
-require("export_helpers__eom_full_loyalty_events")
-require("export_helpers__eom_cult_happiness")
-require("export_helpers__eom_capitulation")
-require("export_helpers__eom_casus_belli")
-require("export_helpers__eom_elector_taxation")
-require("export_helpers__eom_elector_diplomacy")
-require("export_helpers__eom_ui")
-require("export_helpers__eom_chaos_invasion")
-require("export_helpers__eom_revival")
-require("export_helpers__eom_volkmar_unlock")
-require("export_helpers__eom_civil_war_system")
-
-
+require("script/export_helpers__eom_main_events")
+require("script/export_helpers__eom_battles")
+require("script/export_helpers__eom_core")
+require("script/export_helpers__eom_first_turn")
+require("script/export_helpers__eom_plot_events")
+require("script/export_helpers__eom_vlad_civil_wars")
+require("script/export_helpers__eom_full_loyalty_events")
+require("script/export_helpers__eom_cult_happiness")
+require("script/export_helpers__eom_capitulation")
+require("script/export_helpers__eom_casus_belli")
+require("script/export_helpers__eom_elector_taxation")
+require("script/export_helpers__eom_elector_diplomacy")
+require("script/export_helpers__eom_ui")
+require("script/export_helpers__eom_chaos_invasion")
+require("script/export_helpers__eom_revival")
+require("script/export_helpers__eom_volkmar_unlock")
+require("script/export_helpers__eom_civil_war_system")
 require("compat_scripts/export_helpers__eom_zarkis_compat")
