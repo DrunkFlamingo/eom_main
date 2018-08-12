70f5c88d1a6eea7e57bb53d6b6d06b7e0368c662
DrunkFlamingo <matte901@gmail.com>
2018-08-12 13:53:13 -0400

    missing s reeee

--- temp_fixes/export_helpers__stuck_vampires_removal.lua
+++ temp_fixes/export_helpers__stuck_vampires_removal.lua
@@ -1,6 +1,6 @@
 
 local function eom_get_out_of_dkhof_you_cunt()
-    local character_list = cm:get_faction("wh_main_vmp_vampire_count"):character_list()
+    local character_list = cm:get_faction("wh_main_vmp_vampire_counts"):character_list()
     for i = 0, character_list:num_items() - 1 do
         local character = character_list:item_at(i)
         cm:kill_character(character:command_queue_index(), true, true)  
@@ -18,4 +18,5 @@ end
 
 
 
-events.FirstTickAfterWorldCreated[#events.FirstTickAfterWorldCreated+1] = function() eom_get_out_of_dkhof_you_cunt() end;
+events.FirstTickAfterWorldCreated[#events.FirstTickAfterWorldCreated+1] = function() 
+    eom_get_out_of_dkhof_you_cunt() end;
